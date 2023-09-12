8a8af3541c137259284ad08472d50951976b7349
alozar <klimovforever.92@mail.ru>
2023-09-13 01:10:19 +0800

    Выполнен этап "Passing parameters to routes"

--- App.js
+++ App.js
@@ -1,24 +1,70 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { TextInput, Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const HomeScreen = ({ navigation }) => {
+const HomeScreen = ({ route, navigation }) => {
+  React.useEffect(() => {
+    if (route.params?.post) {
+      // Post updated, do something with `route.params.post`
+      // For example, send the post to the server
+    }
+  }, [route.params?.post]);
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
       <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
+        title="Create post"
+        onPress={() => navigation.navigate('CreatePost')}
       />
+      <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
     </View>
   );
 }
 
-const DetailsScreen = ({ navigation }) => {
+const CreatePostScreen = ({ route, navigation }) => {
+  const [postText, setPostText] = React.useState('');
+
+  return (
+    <>
+      <TextInput
+        multiline
+        placeholder="What's on your mind?"
+        style={{ height: 200, padding: 10, backgroundColor: 'white' }}
+        value={postText}
+        onChangeText={setPostText}
+      />
+      <Button
+        title="Done"
+        onPress={() => {
+          // Pass and merge params back to home screen
+          navigation.navigate({
+            name: 'Home',
+            params: { post: postText },
+            merge: true,
+          });
+        }}
+      />
+    </>
+  );
+}
+
+const DetailsScreen = ({ route, navigation }) => {
+  /* 2. Get the param */
+  const { itemId, otherParam } = route.params;
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() =>
+          navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100),
+          })
+        }
+      />
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
       <Button
@@ -34,9 +80,17 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator mode="modal">
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="CreatePost" component={CreatePostScreen} />
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          initialParams={{
+            itemId: 42,
+            otherParam: 'hrum-hrum'
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- README.md
+++ README.md
@@ -19,8 +19,39 @@ npx expo start
 
 ## Команды:
 
+### Навигация:
+
 - `navigation` - prop компонента Screen
 - `navigation.navigate('Details')` - функция маршрутизации по адресу в собственном навигаторе стека
 - `navigation.push('Details')` - функция добавления нового маршрута
 - `navigation.goBack()` - функция возврата в предыдущий адрес
 - `navigation.popToTop()` - функция возврата в корневой адрес
+
+### Передача параметров
+
+- `navigation.navigate('RouteName', { /* params go here */ })` - передача параметров по маршруту
+- `route` - prop компонента Screen, из которого получем параметры
+
+```
+function DetailsScreen({ route, navigation }) {
+    const { itemId, otherParam } = route.params;
+    ...
+```
+
+- Инициализация начальных параметов
+
+```
+<Stack.Screen
+  name="Details"
+  component={DetailsScreen}
+  initialParams={{ itemId: 42 }}
+/>
+```
+
+- Обновление параметров Screen
+
+```
+navigation.setParams({ query: 'someText', ... });
+```
+
+> **Замечание:** Не используйте **setParams** для обновления options Screen, таких как title и т. д. Если вам нужно обновить options, вместо этого используйте **setOptions**.
